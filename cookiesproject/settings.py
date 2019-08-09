a8e7d7d01bc3ba2364206bd721347725aa33f790
cimdinab <47864400+cimdinab@users.noreply.github.com>
2019-08-09 13:55:29 +0300

    Updated setting.py

--- cookiesproject/settings.py
+++ cookiesproject/settings.py
@@ -18,8 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #for static files
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+STATIC_ROOT = (
+    os.path.join(PROJECT_ROOT, 'static')
 )
 
 # Quick-start development settings - unsuitable for production
